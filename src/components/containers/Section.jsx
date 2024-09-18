f1d923b630896fcbd8d36ab1f78b2e9c07399d5e
= <=>
2024-09-18 13:56:46 -0600

    init project

---
+++ src/components/containers/Section.jsx
@@ -0,0 +1,22 @@
+import "./main.css"
+
+export const SectionOne = () => {
+  return(
+    <section className="flex flex-column">
+      <article className="flex-item">
+        <h5 className="title title-lg">Happy Birthday</h5>
+        <h1 className="title title-xl">Jennifer!</h1>
+      </article>
+      <article className="">
+          <i className="">heart</i>
+          <i className="">heart</i>
+          <i className="">heart</i>
+          <i className="">heart</i>
+          <i className="">heart</i>
+          <i className="">heart</i>
+          <i className="">heart</i>
+          <i className="">heart</i>
+      </article>
+    </section>
+  )
+}
