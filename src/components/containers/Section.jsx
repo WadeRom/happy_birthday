9e495f0b5185215c149be1f0cbb5bf0d7616a080
= <=>
2024-09-19 12:58:12 -0600

    Fix: compress images for app

--- src/components/containers/Section.jsx
+++ src/components/containers/Section.jsx
@@ -1,11 +1,11 @@
-import "./main.css"
+import "./main.css";
 
 export const SectionOne = () => {
   return(
-    <section className="flex flex-column">
-      <article className="flex-item">
-        <h5 className="title title-lg">Happy Birthday</h5>
-        <h1 className="title title-xl">Jennifer!</h1>
+    <section className="p-md">
+      <article className="">
+        <h5 className="">Happy Birthday</h5>
+        <h1 className="">Jennifer!</h1>
       </article>
       <article className="">
           <i className="">heart</i>
