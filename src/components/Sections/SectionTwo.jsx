61a8041099f1945c191cff468bbac39c7120d5bb
= <=>
2024-09-22 19:58:26 -0600

    refactor: code reorganization, moved the animation state to a custom hook

--- src/components/Sections/SectionTwo.jsx
+++ src/components/Sections/SectionTwo.jsx
@@ -1,48 +1,31 @@
-import './index.css';
-import { useState, useEffect } from 'react';
 import poems from "../../api/poems.json";
+import useCarouselAnimation from "../../hooks/useCarouselAnimation";
+
 
 const Carousel = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [slideDirection, setSlideDirection] = useState(''); // Controla la dirección de la animación
-  const [isAnimating, setIsAnimating] = useState(false);    // Para controlar la animación
+  const {carouselState, setCarouselAnimation} = useCarouselAnimation()
+  const {index, animation} =  carouselState;
 
-  const nextPoem = () => {
-    if (isAnimating) return; // Evita múltiples clics mientras hay una animación
-    setSlideDirection('next');
-    setIsAnimating(true);
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % poems.length);
-  };
+  const next = (index) => (index + 1) % poems.length;
+  const prev = (index) => (index - 1 + poems.length) % poems.length;
 
-  const prevPoem = () => {
-    if (isAnimating) return;
-    setSlideDirection('prev');
-    setIsAnimating(true);
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + poems.length) % poems.length);
-  };
-
-  // useEffect para manejar el inicio y fin de la animación
-  useEffect(() => {
-    if (isAnimating) {
-      const animationTimeout = setTimeout(() => {
-        setIsAnimating(false); // Finalizar animación después de 500ms
-        setSlideDirection('');  // Resetear dirección de la animación
-      }, 500); // Duración de la animación en milisegundos
-
-      return () => clearTimeout(animationTimeout); // Limpiar temporizador si el componente se desmonta
-    }
-  }, [isAnimating]);
+  const nextPoem = () => setCarouselAnimation(next, "slideInFromRight");
+  const prevPoem = () => setCarouselAnimation(prev, "slideInFromLeft");
 
   return (
-    <div className={`carousel-container ${slideDirection} ${isAnimating ? 'animating' : ''}`}>
-      <div className="poem">
-        <h2>{poems[currentIndex].title}</h2>
-        <p>{poems[currentIndex].content}</p>
-        <p>Author: {poems[currentIndex].author}</p>
-        <p>Fecha: {poems[currentIndex].fecha}</p>
+    <div className="overflow-hidden h-75 m-auto">
+      <h2 className="font-title--lg color-yellow text-center">
+        {poems[index].title}
+      </h2>
+      <div
+        className={`w-full w-max--md m-auto ${animation}`}
+      >
+        <p className="text-center">{poems[index].content}</p>
+        <p>Author: {poems[index].author}</p>
+        <p>Fecha: {poems[index].fecha}</p>
+        <button onClick={prevPoem} aria-label="Previous Poem">Prev</button>
+        <button onClick={nextPoem} aria-label="Next Poem">Next</button>
       </div>
-      <button onClick={prevPoem} disabled={isAnimating}>Previous</button>
-      <button onClick={nextPoem} disabled={isAnimating}>Next</button>
     </div>
   );
 };
@@ -50,7 +33,9 @@ const Carousel = () => {
 export const SectionTwo = () => {
   return (
     <section id="2" className="w-full  h-full color-white">
-      <Carousel/>
+      <article className="w-max--lg w-min--xs m-auto border h-full flex justify-between align-start ">
+        <Carousel />
+      </article>
     </section>
   );
 };
