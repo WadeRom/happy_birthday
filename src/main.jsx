ab881b187a8f486e7649fe8faf79ce3e8082ac0a
= <=>
2024-09-22 21:55:44 -0600

    refact: add strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')).render(
-
-    <App />
+    <StrictMode>
+        <App />
+    </StrictMode>
 )
