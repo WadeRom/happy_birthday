234918142558b068a0c28a9e8dc9da4de8d32492
= <=>
2024-09-24 12:04:19 -0600

    rename of file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
+  server:{
+    host:true,
+    port:5173,
+  },
   plugins: [react(), svgr()],
 });
