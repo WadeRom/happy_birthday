3f498056adb4407126abe94080e562750763495a
= <=>
2024-09-25 20:45:45 -0600

    refact: change strucutre of properties and css clases was added

--- src/components/Carousel/CarouselContent.jsx
+++ src/components/Carousel/CarouselContent.jsx
@@ -2,12 +2,11 @@ import { formatContent } from "../../utils/carouselUtils";
 import { useResetScroll } from "../../hooks/useResetScroll";
 
 const CarouselContent = ({ content, animation }) =>{
-  const scrollRef = useResetScroll();
-
+  const scrollRef = useResetScroll([content]);
   const formattedContent = formatContent(content);
   return (
     <p className={`h-max--90 w-full color-white w-full overflow-y--auto font-poppins font-normal scrollbar-none ${animation}`} ref={scrollRef}>
-      <em className="white-space--pre  text-center flex flex-col gap-xs ">
+      <em className="white-space--pre text-center flex flex-col gap-xs ">
         {formattedContent}
       </em>
     </p>
