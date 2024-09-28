c7e0e9bb74d314cc7eb0977b3dafea53d5e7ddaf
= <=>
2024-09-27 22:33:42 -0600

    feature: initial commit for this section

---
+++ src/components/Sections/SectionFour.jsx
@@ -0,0 +1,21 @@
+import { Title } from "../Typography/Typography"
+
+export const SectionFour = () => 
+{
+  return (
+    <section>
+      <article>
+        <Title>Vicent van Gogh</Title>
+        <p>
+          Estas son las obras más reconocidas de Van Gogh. Espero que te gusten y que las disfrutes tanto como yo. 🌻✨ Con mucho cariño, Walter.
+        </p>
+        <p>
+          P.D.: ¡Feliz cumpleaños! 🎂🎉 XOXO 💖
+        </p>
+      </article>
+      <article>
+        
+      </article>
+    </section>
+  )
+}
