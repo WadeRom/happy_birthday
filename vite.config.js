c2b0660680cd00144a93b74288080b1ab1aaec63
= <=>
2024-10-01 21:26:06 -0600

    configure deploy and fix css file

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-    server:{
-      host:true,
-      port:5173,
-    },
   plugins: [react(), svgr()],
+  base: 'https://waderom.github.io/happy_birthday'
 });
